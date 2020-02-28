05dedd12c33bdfbca9e920475abe3b8ad235d5bd
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-02-28 21:28:51 +0000

    Renamed the vue component

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="text-white mb-8">
+        <div class="places-input text-gray-800">
+            <input type="text" class="w-full">
+        </div>
+
+        <div class="weather-container font-sans w-128 max-w-lg overflow-hidden rounded-lg bg-gray-900 shadow-lg mt-4">
+            <div class="current-weather flex items-center justify-between px-6 py-8">
+                <div class="flex items-center">
+                    <div>
+                        <div class="text-6xl font-semibold">{{ currentTemperature.actual }}°C</div>
+                        <div>Feels like {{ currentTemperature.feels }} °C</div>
+                    </div>
+                    <div class="mx-5">
+                        <div class="semi-bold">{{ currentTemperature.summary }}</div>
+                        <div>{{ location.name }}</div>
+                    </div>
+                </div>
+                <div>
+                    <canvas id="iconCurrent" width="96" height="96"></canvas>
+                </div>
+            </div> <!-- end of current weather -->
+            <div class="weekly-weather text-sm bg-gray-800 px-6 py-8 overflow-hidden">
+                <div v-for="(day, index) in daily" 
+                    :key="day.time" 
+                    class="flex items-center"
+                    :class="{ 'mt-8' : index > 0 }"
+                    v-if="index < 5"
+                >
+                    <div class="w-1/6 text-lg text-gray-200">{{ toDayOfWeek(day.time) }}</div>
+                    <div class="w-4/6 px-4 flex items-center">
+                        <div>
+                            <canvas :id="`icon${index+1}`" :data-icon="day.icon" width="24" height="24"></canvas>
+                        </div>
+                        <div class="ml-3">{{ day.summary }}</div>
+                    </div>
+                    <div class="w-1/6 text-right">
+                        <div>{{ Math.round(day.temperatureHigh) }}°C</div>
+                        <div>{{ Math.round(day.temperatureLow) }}°C</div>
+                    </div>
+                </div>
+
+            </div> <!-- end of weekly-weather -->
+        </div> <!-- end of weather container -->
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            this.fetchData()
+        },
+
+        data() {
+            return {
+                currentTemperature: {
+                    actual: '',
+                    feels: '',
+                    summary: '',
+                    icon: ''
+                },
+
+                daily: [],
+
+                location: {
+                    name: 'London, UK',
+                    lat: 51.5074,
+                    lng: 0.1278
+                }
+            }
+        },
+
+        methods: {
+            fetchData() {
+                var skycons = new Skycons({'color': 'white'});
+
+                fetch(`/api/weather?lat=${this.location.lat}&lng=${this.location.lng}`)
+                    .then(response => response.json())
+                    .then(data => {
+                         console.log(data);
+                        this.currentTemperature.actual = Math.round(data.currently.temperature);
+                        this.currentTemperature.feels = Math.round(data.currently.apparentTemperature);
+                        this.currentTemperature.summary = data.currently.summary;
+                        this.currentTemperature.icon = data.currently.icon;
+
+                        this.daily = data.daily.data;
+
+                        skycons.add("iconCurrent", this.currentTemperature.icon);
+                        skycons.play();
+
+                        this.$nextTick(() => {
+                            skycons.add('icon1', document.getElementById('icon1').getAttribute('data-icon'));
+                            skycons.add('icon2', document.getElementById('icon2').getAttribute('data-icon'));
+                            skycons.add('icon3', document.getElementById('icon3').getAttribute('data-icon'));
+                            skycons.add('icon4', document.getElementById('icon4').getAttribute('data-icon'));
+                            skycons.add('icon5', document.getElementById('icon5').getAttribute('data-icon'));
+                            skycons.play();
+                        })
+                    })
+            },
+
+            toDayOfWeek(timestamp) {
+                const newDate = new Date(timestamp * 1000);
+                const days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+
+                return days[newDate.getDay()]
+            }
+        }
+    }
+</script>
